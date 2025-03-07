5abec546ec8e9c2c843c18883218b728a1c20431
evgen <evgeni.veselov@gmail.com>
2025-03-07 20:28:42 +0300

    all

--- run.ps1
+++ run.ps1
@@ -1,34 +1,45 @@
-# Прочитать строки из файла
-$lines = Get-Content -Path "questions.txt"
+function Process-QuestionsFile {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$FileName
+    )
 
-# Инициализация переменных
-$data = @()
-$currentQuestion = $null
-$questionNumber = 0
+    # Прочитать строки из указанного файла
+    $lines = Get-Content -Path $FileName
 
-# Обработка строк файла
-foreach ($line in $lines) {
-    if (-not [string]::IsNullOrWhiteSpace($line)) {
-        if ($line -match "^\d+\.(.+)$") {
-            if ($currentQuestion -ne $null) {
-                $data += $currentQuestion
+    # Инициализация переменных
+    $data = @()
+    $currentQuestion = $null
+    $questionNumber = 0
+
+    # Обработка строк файла
+    foreach ($line in $lines) {
+        if (-not [string]::IsNullOrWhiteSpace($line)) {
+            if ($line -match "^\d+\.(.+)$") {
+                if ($currentQuestion -ne $null) {
+                    $data += $currentQuestion
+                }
+                $questionNumber++
+                $currentQuestion = @{
+                    n = $questionNumber
+                    q = $matches[1].Trim()
+                    a = @()
+                }
+            } elseif ($currentQuestion -ne $null) {
+                $currentQuestion.a += $line.Trim()
             }
-            $questionNumber++
-            $currentQuestion = @{
-                n = $questionNumber
-                q = $matches[1].Trim()
-                a = @()
-            }
-        } elseif ($currentQuestion -ne $null) {
-            $currentQuestion.a += $line.Trim()
         }
     }
+
+    if ($currentQuestion -ne $null) {
+        $data += $currentQuestion
+    }
+
+    # Преобразование данных в формат JSON и сохранение в файл
+    $outputFile = [System.IO.Path]::ChangeExtension($FileName, "json")
+    $data | ConvertTo-Json -Depth 3 | Set-Content -Path $outputFile
+    Write-Output "Данные успешно сохранены в файл $outputFile"
 }
 
-if ($currentQuestion -ne $null) {
-    $data += $currentQuestion
-}
-
-# Преобразование данных в формат JSON и сохранение в файл
-$data | ConvertTo-Json -Depth 3 | Set-Content -Path "questions.json"
-Write-Output "Данные успешно сохранены в файл questions.json"
+Process-QuestionsFile -FileName "questions_common.txt"
+Process-QuestionsFile -FileName "questions_cosmetology.txt"
